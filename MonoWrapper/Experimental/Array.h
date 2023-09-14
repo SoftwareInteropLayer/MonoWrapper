d56556d6baedcc9f39145ca9172998f8c51bbd2c
Chaika9 <bestdevelop22@gmail.com>
2023-09-14 18:11:45 +0900

    Add Array get function

--- MonoWrapper/Experimental/Array.h
+++ MonoWrapper/Experimental/Array.h
@@ -42,7 +42,30 @@ namespace Mono::Experimental {
          */
         int count() const;
 
-        template<typename T>
+        /**
+         * @brief Get element at specified index. (Only Object type)
+         * @tparam T Type of element.
+         * @param index Index of element.
+         * @return Element at specified index.
+         * @throws std::out_of_range If index is out of range.
+         */
+        template<typename T, typename = std::enable_if_t<std::is_same_v<T, Object>>>
+        T get(int index) const {
+            if (index < 0 || index >= _count) {
+                throw std::out_of_range("index out of range");
+            }
+            auto element = mono_array_get(_array, T, index);
+            return ConvertType<std::decay_t<T>>::fromMono(element);
+        }
+
+        /**
+         * @brief Set element at specified index. (Only Object type)
+         * @tparam T Type of element.
+         * @param index Index of element.
+         * @param value Value to set.
+         * @throws std::out_of_range If index is out of range.
+         */
+        template<typename T, typename = std::enable_if_t<!std::is_same_v<T, Object>>>
         void set(int index, const T &value) const {
             if (index < 0 || index >= _count) {
                 throw std::out_of_range("index out of range");
