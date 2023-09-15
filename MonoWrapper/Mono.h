85fb57fad2064f2567a32f27b9ccd04ab95f8c6b
Chaika9 <bestdevelop22@gmail.com>
2023-09-15 16:46:32 +0900

    Add Mono header (general include header)

---
+++ MonoWrapper/Mono.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "Activator.h"
+#include "AppDomain.h"
+#include "Assembly.h"
+#include "Type.h"
+#include "Object.h"
+#include "MethodInfo.h"
+#include "MethodInvoker.h"
+#include "MethodInternal.h"
+#include "FieldInfo.h"
+#include "PropertyInfo.h"
+#include "Thread.h"
+
+// Experimental
+#include "Experimental/Array.h"
